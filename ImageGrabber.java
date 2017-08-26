64af1190f379ecbb80011777ad3c707c5f8a469d
Furkan Akal <30433306+furkan-dev@users.noreply.github.com>
2017-08-26 21:35:29 +0300

    Add files via upload

---
+++ ImageGrabber.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class ImageGrabber {
+	
+	public static void main(String args[]) throws IOException{
+		
+		Document doc;
+		doc = Jsoup.connect("http://720pizle.com/power-rangers.html").get();
+		
+		Elements images = doc.select("img[src~=(?i)\\.(png|jpe?g|gif)]");
+		for(Element image: images) {
+			
+			String l = image.attr("src");
+			if(l.length()>0){
+				if(l.length()<4)
+					l = doc.baseUri()+l.substring(1);
+				else if(!l.substring(0, 4).equals("http"))
+					l = doc.baseUri()+l.substring(1);			
+			}
+		
+			System.out.println(l);
+		}
+	}
+}
---
+++ WebsiteParser.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class WebsiteParser {
+	
+	public static void main(String args[]) throws IOException{
+		
+		Document doc;
+		doc = Jsoup.connect("http://720pizle.com/").get();
+		
+		Elements links = doc.getElementsByTag("a");
+		for(Element link: links) {
+			
+			String l = link.attr("href");
+			if(l.length()>0){
+				if(l.length()<4)
+					l = doc.baseUri()+l.substring(1);
+				else if(!l.substring(0, 4).equals("http"))
+					l = doc.baseUri()+l.substring(1);			
+			}
+		
+			System.out.println(l);
+		}
+	}
+}
